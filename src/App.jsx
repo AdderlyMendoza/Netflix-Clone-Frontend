2dccdb835e4c0fb6fe73fb3a06577cca048652d9
AdderlyMendoza <aderly19xd@ejemplo.com>
2025-02-06 10:41:55 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Carrusell from './components/Carrusell'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Carrusell />
+    </>
+  )
+  
+}
+
+export default App
---
+++ src/components/Carrusell.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Carrusell () {
+  return (
+    <header>
+      <h1>Carrusell</h1>
+    </header>
+  )
+}
+
+export default Carrusell;
